5763409ddb7781b2e0da2feac4d76a77ce421104
JackDan9 <1835812864@qq.com>
2018-07-26 10:02:26 +0800

    :cn: python : scrapy : spiders : mygirl(town)

--- mygirl/spiders/mygirl.py
+++ mygirl/spiders/mygirl.py
@@ -10,214 +10,91 @@ import traceback
 class MyGirlSpider(scrapy.Spider):
     name = "mygirl"
     start_urls = [
-        "http://www.weather.com.cn/weather1d/101180101.shtml",
+        "http://forecast.weather.com.cn/town/weather1dn/101260409003.shtml", # 贵州省-平塘县-大塘镇
     ]
 
     def parse(self, response):
-        Tod_Weather_Date = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[1]/h1/text()').extract()
-        Tod_Weather_Wea = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[1]/p[1]/text()').extract()
-        Tod_Weather_Tem = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[1]/p[2]/span/text()').extract()
-        Tod_Weather_Wind_Con = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[1]/p[3]/span/@title').extract()
-        Tod_Weather_Wind_Num = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[1]/p[3]/span/text()').extract()
+        Tod_Weather_Wea = response.xpath('//*[@class="weather dis"]/text()').extract()
+        Tod_Weather_Tem = response.xpath('//*[@class="tempDiv"]/span[1]/text()').extract()
+        Tod_Weather_Tem_Max = response.xpath('//*[@id="maxTempDiv"]/span/text()').extract()
+        Tod_Weather_Tem_Min = response.xpath('//*[@id="minTempDiv"]/span/text()').extract()
+        Tod_Weather_Con_Num = response.xpath('//*[@class="todayLeft"]/p[1]/span/text()').extract()
+        Tod_Weather_Hum = response.xpath('//*[@class="todayLeft"]/p[2]/span/text()').extract()
 
-        Tom_Weather_Date = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[2]/h1/text()').extract()
-        Tom_Weather_Wea = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[2]/p[1]/text()').extract()
-        Tom_Weather_Tem = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[2]/p[2]/span/text()').extract()
-        Tom_Weather_Wind_Con = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[2]/p[3]/span/@title').extract()
-        Tom_Weather_Wind_Num = response.xpath('//*[@id="today"]/div[@class="t"]/ul/li[2]/p[3]/span/text()').extract()
+        Tod_Weather_Ult_Ray = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[1]/dt/em/text()').extract()
+        Tod_Weather_Ult_Ray_Sug = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[1]/dd/text()').extract()
+        Tod_Weather_Cold = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[2]/dt/em/text()').extract()
+        Tod_Weather_Cold_Sug = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[2]/dd/text()').extract()
+        Tod_Weather_Dress = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[3]/dt/em/text()').extract()
+        Tod_Weather_Dress_Sug = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[3]/dd/text()').extract()
+        Tod_Weather_Exercise = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[5]/dt/em/text()').extract()
+        Tod_Weather_Exercise_Sug = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[5]/dd/text()').extract()
+        Tod_Weather_Pollute = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[6]/dt/em/text()').extract()
+        Tod_Weather_Pollute_Sug = response.xpath('//*[@class="weather_shzs weather_shzs_1d"]/div[@class="lv"]/dl[6]/dd/text()').extract()
+        # Hum(Humidity)
 
         today = datetime.datetime.today()
+        print(today)
         anniversary = datetime.datetime(2018, 3, 14)
         loving_days = (today - anniversary).days
 
-        # if('雨' in Tod_Weather_Wea[0]):
-        if((Tod_Weather_Wea[0].find(u'雨')) != -1):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s(记得带伞哦)\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                   today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0], Tod_Weather_Tem[0],
-                   Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                   Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        # elif('雨' in Tom_Weather_Wea[0]):
-        elif((Tom_Weather_Wea[0].find(u'雨')) != -1):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s(记得带伞哦)\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        # elif(('雨' in Tod_Weather_Wea[0]) and ('雨' in Tom_Weather_Wea[0])):
-        elif(((Tod_Weather_Wea[0].find(u'雨')) != -1) and ((Tom_Weather_Wea[0].find(u'雨')) != -1)):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s(记得带伞哦)\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s(记得带伞哦)\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif(28 <= int(Tod_Weather_Tem[0]) < 50):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃(记得涂防晒霜哦)\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                   today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0], Tod_Weather_Tem[0],
-                   Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                   Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif (-15 <= int(Tod_Weather_Tem[0]) < 10):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃(记得穿厚些哦)\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif(28 <= int(Tom_Weather_Tem[0]) < 50):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃(记得涂防晒霜哦)\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif(-15 <= int(Tom_Weather_Tem[0]) < 10):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃(记得穿厚一些哦)\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif((28 <= int(Tod_Weather_Tem[0]) < 50) and (28 <= int(Tom_Weather_Tem[0]) < 50)):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃(记得涂防晒霜)\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃(记得涂防晒霜)\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        elif((-15 <= int(Tod_Weather_Tem[0]) < 10) and (-15 <= int(Tom_Weather_Tem[0]) < 10)):
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃(记得穿厚一点哦)\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃(记得穿厚一点哦)\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (
-                       today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0],
-                       Tod_Weather_Tem[0],
-                       Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0],
-                       Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-        else:
-            lst = [u'你好，呆瓜:\n\n\t'
-                   u'今天是%s\n\t '
-                   u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
-                   u'今天日期:%s\n'
-                   u'今天天气情况:%s\n'
-                   u'今天温度:%s℃\n'
-                   u'今天风向:%s\n'
-                   u'今天风的级数:%s\n\n'
-                   u'明天日期:%s\n'
-                   u'明天天气情况:%s\n'
-                   u'明天温度:%s℃\n'
-                   u'明天风向:%s\n'
-                   u'明天风的级数:%s\n\n' % (today.strftime('%Y-%m-%d'), loving_days, Tod_Weather_Date[0], Tod_Weather_Wea[0], Tod_Weather_Tem[0], Tod_Weather_Wind_Con[0], Tod_Weather_Wind_Num[0], Tom_Weather_Date[0], Tom_Weather_Wea[0], Tom_Weather_Tem[0], Tom_Weather_Wind_Con[0], Tom_Weather_Wind_Num[0])]
-
+        lst = [u'你好，呆瓜:\n\n\t'
+               u'今天是%s, 地点是平塘县-大塘镇(西关中学和西关小学)\n\t'
+               u'首先，今天已经是我们相恋的第%s天了喔。然后大兵就要来播送天气预报了！！\n\n\t'
+               u'今天日期:%s\n'
+               u'今天天气情况:%s\n'
+               u'今天温度:%s℃\n'
+               u'今天最高气温:%s\n'
+               u'今天最低气温:%s\n'
+               u'今天风向和风的级数:%s\n'
+               u'今天相对湿度:%s\n\n'
+               
+               u'大兵的生活小贴士:\n'
+               u'紫外线:%s\n'
+               u'小贴士:%s\n'
+               u'感冒:%s\n'
+               u'小贴士:%s\n'
+               u'穿衣:%s\n'
+               u'小贴士:%s\n'
+               u'运动:%s\n'
+               u'小贴士:%s\n'
+               u'空气污染扩散:%s\n'
+               u'小贴士:%s\n\n'
+        
+               u'爱你哟!!!'
+               % (
+                    today.strftime('%Y-%m-%d'), 
+                    loving_days, 
+                    today.strftime('%Y-%m-%d'), 
+                    Tod_Weather_Wea[0], 
+                    Tod_Weather_Tem[0], 
+                    Tod_Weather_Tem_Max[0], 
+                    Tod_Weather_Tem_Min[0], 
+                    Tod_Weather_Con_Num[0], 
+                    Tod_Weather_Hum[0], 
+                    Tod_Weather_Ult_Ray[0], 
+                    Tod_Weather_Ult_Ray_Sug[0], 
+                    Tod_Weather_Cold[0], 
+                    Tod_Weather_Cold_Sug[0], 
+                    Tod_Weather_Dress[0], 
+                    Tod_Weather_Dress_Sug[0], 
+                    Tod_Weather_Exercise[0], 
+                    Tod_Weather_Exercise_Sug[0], 
+                    Tod_Weather_Pollute[0], 
+                    Tod_Weather_Pollute_Sug[0]
+                )
+            ]
         # It is receiver email word.
-        mailto_list = "*********@qq.com"
+        mailto_list = "**********@qq.com"
         mail_host = "smtp.qq.com"
         # It is your email word.
-        mail_user = "********@qq.com"
+        mail_user = "**********@qq.com"
         # It is your password
-        mail_pass = "**********"
+        mail_pass = "****************"
 
 
         content = ''.join(lst)
+        print(content)
         msg = MIMEText(content, _subtype='plain', _charset='utf-8')
         msg['From'] = mail_user
         msg['To'] = mailto_list
@@ -229,4 +106,3 @@ class MyGirlSpider(scrapy.Spider):
             s.close()
         except Exception as e:
             print(e)
-
